ec6d4125c10fa2bb36639bd41b07d4f6093edd98
Janae-Waldon <janae.waldon@gmail.com>
2018-06-20 00:52:17 -0700

    update to menu
    menu functions better now... wrap up comments in am (need to finish third sub menu inside of write file)...sheesh.

--- a2.c
+++ a2.c
@@ -1,5 +1,11 @@
 #include "a2.h"
 
+int actionPerformed;
+
+char userFilePath[] = " ";
+char userDirectoryName[] = " ";
+char userFilename[] = " ";
+
 void createfile(char* filename){
     FILE *fp;
     fp=fopen(filename,"w");
@@ -127,133 +133,241 @@ void menu(){
     printf("Hello, what would you like to do?\n");
     printf("1. Create a new directory\n");
     printf("2. Create a new file\n");
-    printf("3. Write to a file\n");
-    printf("4. Read/Write to shadow file\n");
-    printf("5. Read an exitsing file\n");
-    printf("6. Write to a existing file\n");
-    printf("7. Print file status\n");
-    printf("8. Print directory listing\n");
-    printf("9. Exit\n");
-    printf("0. Help\n");
+    printf("3. Read a file\n");
+    printf("4. Write to a file\n");
+    printf("5. Print file status\n");
+    printf("6. Print directory listing\n");
+    printf("0. Exit\n");
 }
 
-void performMenuInstruction(){
+void performMenuInstructions(){
     int subMenuOption;
+    int subMenuOption_2;
     
     do{
-        //0. Gives the user instructions how the main menu works.
-        if(actionPerformed == 0){
-            printf("On the command line, type the number that corresponds to the action you want to be performed. For Example, if you would like to write to a file, you would type '6'. (Note only one parameter is excepted at a time.)\n For name, type in whatever filename or directory name you want. For example, when prompted for name, type 'Myfile' or 'Directory Name'.\n");
-        }
+        menu();
         
-        //1. creates a sub menu that exits/ goes back to previous menu & creates a new directory file using user input.
-        else if(actionPerformed == 1){
-            int subActionPerformed;
-            
-            //submenu
-            printf("1. New directory\n");
+        scanf("%d", &actionPerformed);
+//        if(!(isdigit(actionPerformed)) ){
+//            printf("ENTER A NUMBER BETWEEN 0-9\n");
+//            break;
+//        }
+        
+        /* 1. Create a new directory:
+         creates a sub menu that exits/ goes back to previous menu & creates a new directory file using user input. */
+        if(actionPerformed == 1){
+        
+            printf("1. Create new directory\n");
             printf("2. Back\n");
-            printf("9. Exit\n");
+            printf("0. Exit\n");
             
-            scanf("%d", &subActionPerformed);
+            scanf("%d", &subMenuOption);
             if(subMenuOption == 1){
-                printf("Filename: ");
+                printf("Directory filename: ");
                 scanf("%s", userDirectoryName);
                 createDirectory(userDirectoryName);
             }
-            else if(subMenuOption == 2){
-                continue;                                               //go to main menu
-            }
-            else if(subMenuOption == 9){
+            if(subMenuOption == 0){
                 break;
             }
-//            else{
-//                printf("Not an option");
-//                continue;
-//            }
-            //return;
+            if(subMenuOption == 2){
+                continue;
+            }
+            else{
+                printf("ENTER 1, 2, or 0\n");
+                continue;
+            }
         }
-        /*
-         //2. creates a new file using user input; additionally creates a sub menu that exits/ goes back to previous menu
+        
+        /* 2. Create a new file:
+            creates a sub menu to create a new file. Includes the ability to go back to the main menu, exit, create a file by passing user input for filename & path. */
          else if(actionPerformed == 2){
-         printf("New filename: ");
-         scanf("%s", userFilename);
-         createfile(userFilename);
-         
-         //submenu
-         printf("0. Back\n");
-         printf("1. Exit\n");
-         
-         if(subMenuOption == 0){
-         //menu();
+             
+             printf("1. Create new file\n");
+             printf("2. Back\n");
+             printf("0. Exit\n");
+             
+             scanf("%d", &subMenuOption);
+             if(subMenuOption == 1){
+                 printf("File path: ");
+                 scanf("%s", userFilePath);
+                 //get path of directory (open)
+                 //if (filepath = NULL)
+                 //do{
+                     //printf("Directory doen't exist!\n");
+                 //} while (filepath = NULL);
+                 //continue;
+                 //cast to directory path
+                 printf("New filename: ");
+                 scanf("%s", userFilename);
+                 createfile(userFilename);
+             }
+             if(subMenuOption == 0){
+                 break;
+             }
+             if(subMenuOption == 2){
+                 continue;
+             }
+             else{
+                 printf("ENTER 1, 2, or 0\n");
+                 continue;
+             }
          }
-         else if(subMenuOption == 1){
-         //exit program
-         break;
+ 
+         /*3. Read an existing file
+            another submenu that reads & opens file and prints the contents to stdout */
+         else if(actionPerformed == 3){
+             
+             printf("1. Read a file\n");
+             printf("2. Back\n");
+             printf("0. Exit\n");
+             
+             scanf("%d", &subMenuOption);
+             if(subMenuOption == 1){
+                 printf("File path: ");
+                 scanf("%s", userFilePath);
+                 //get path of directory (open)
+                 //if (filepath = NULL)
+                 //do{
+                 //printf("Directory doen't exist!\n");
+                 //} while (filepath = NULL);
+                 //continue;
+                 //cast to directory path
+                 scanf("%s", userFilename);
+                 printf("Contents of file: \n");
+                 //printf("%", readfile(userFilename));
+             }
+             if(subMenuOption == 0){
+                 break;
+             }
+             if(subMenuOption == 2){
+                 continue;
+             }
+             else{
+                 printf("ENTER 1, 2, or 0\n");
+                 continue;
+             }
          }
-         else{
-         printf("Not an option");
-         break;
-         }
-         
-         
-         
-         //write to a file
-         case 3:
-         printf("Filename: ");
-         scanf("%s", userFilename);
-         writefile(userFilename);
-         break;
-         
-         //read/write to shadow
-         case 4:
-         printf("                     WHAT?                            ");
-         //scan & store
-         //shadow();
-         break;
-         
-         //read an existing file
-         case 5:
-         printf("Filename: ");
-         //scan & store
-         //readfile(userFilename);
-         readfile(userFilename);
-         break;
-         
+/* Finish this in the morning
+ 
          //write to an existing file (in insert, append, or overwite mode)
-         case 6:
+         else if(actionPerformed == 4){
+             
+             
+             printf("1. Write to a file\n");
+             printf("2. Back\n");
+             printf("0. Exit\n");
+             
+             scanf("%d", &subMenuOption);
+             if(subMenuOption == 1){
+                 printf("File path: ");
+                 scanf("%s", userFilePath);
+                 //get path of directory (open)
+                 //if (filepath = NULL)
+                 //do{
+                 //printf("Directory doen't exist!\n");
+                 //} while (filepath = NULL);
+                 //continue;
+                 //cast to directory path
+                 
+                 printf("1. Append\n");
+                 printf("2. Insert\n");
+                 printf("0. Overrite\n");
+                 printf("2. Back\n");
+                 printf("0. Exit\n");
+                 
+                 scanf("%s", userFilename);
+                 printf("Contents of file: \n");
+                 //printf("%", readfile(userFilename));
+             }
+             if(subMenuOption == 0){
+                 break;
+             }
+             if(subMenuOption == 2){
+                 continue;
+             }
+             else{
+                 printf("ENTER 1, 2, or 0\n");
+                 continue;
+             }
+         }
+    
+        case 6:
          printf("Filename: ");
          //scan & store
          writefile(userFilename);
          break;
-         
+*/
          //print file status
-         case 7:
-         printf("Filename: ");
-         
-         //scan & store
-         printfilestatus(userFilename);
-         break;
-         
-         //print directory listing
-         case 8:
-         printf("Directory name: ");
-         
-         printdirlisting(userFilename);
-         break;
-         
-         default:
-         printf("Please enter one number between 0-9.\n");
+         else if(actionPerformed == 5){
+             
+             printf("1. Print File Status\n");
+             printf("2. Back\n");
+             printf("0. Exit\n");
+             
+             scanf("%d", &subMenuOption);
+             if(subMenuOption == 1){
+                 printf("File path: ");
+                 scanf("%s", userFilePath);
+                 //get path of directory (open)
+                 //if (filepath = NULL)
+                 //do{
+                 //printf("Directory doen't exist!\n");
+                 //} while (filepath = NULL);
+                 //continue;
+                 //cast to directory path
+                 scanf("%s", userFilename);
+                 printfilestatus(userFilename);
+             }
+             if(subMenuOption == 0){
+                 break;
+             }
+             if(subMenuOption == 2){
+                 continue;
+             }
+             else{
+                 printf("ENTER 1, 2, or 0\n");
+                 continue;
+             }
          }
-         */
-    } while (actionPerformed != 9 && isdigit(actionPerformed));
+        
+         //print directory listing
+         else if(actionPerformed == 6){
+             
+             printf("1. Print File Status\n");
+             printf("2. Back\n");
+             printf("0. Exit\n");
+             
+             scanf("%d", &subMenuOption);
+             if(subMenuOption == 1){
+                 printf("File path: ");
+                 scanf("%s", userFilePath);
+                 //get path of directory (open)
+                 //if (filepath = NULL)
+                 //do{
+                 //printf("Directory doen't exist!\n");
+                 //} while (filepath = NULL);
+                 //continue;
+                 //cast to directory path
+                 scanf("%s", userFilename);
+                 printdirlisting(userFilename);
+             }
+             if(subMenuOption == 0){
+                 break;
+             }
+             if(subMenuOption == 2){
+                 continue;
+             }
+             else{
+                 printf("ENTER 1, 2, or 0\n");
+                 continue;
+             }
+         }
+*/
+    } while (actionPerformed != 0);
 }
     
 int main(int argc, char const *argv[]){
-        
-            menu();
-            scanf("%d", &actionPerformed);
-            performMenuInstruction();
-            
-   
+    performMenuInstructions();
+    return 0;
 }
