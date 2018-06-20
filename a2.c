dc1369986aa186fbcadeee43daa2faba65462019
Janae-Waldon <janae.waldon@gmail.com>
2018-06-19 21:35:55 -0700

    add menu
    added the menu, along with operations i have in the main... still in progress...

--- a2.c
+++ a2.c
@@ -123,3 +123,137 @@ void reversesortalpha(){
     
 }
 
+void menu(){
+    printf("Hello, what would you like to do?\n");
+    printf("1. Create a new directory\n");
+    printf("2. Create a new file\n");
+    printf("3. Write to a file\n");
+    printf("4. Read/Write to shadow file\n");
+    printf("5. Read an exitsing file\n");
+    printf("6. Write to a existing file\n");
+    printf("7. Print file status\n");
+    printf("8. Print directory listing\n");
+    printf("9. Exit\n");
+    printf("0. Help\n");
+}
+
+void performMenuInstruction(){
+    int subMenuOption;
+    
+    do{
+        //0. Gives the user instructions how the main menu works.
+        if(actionPerformed == 0){
+            printf("On the command line, type the number that corresponds to the action you want to be performed. For Example, if you would like to write to a file, you would type '6'. (Note only one parameter is excepted at a time.)\n For name, type in whatever filename or directory name you want. For example, when prompted for name, type 'Myfile' or 'Directory Name'.\n");
+        }
+        
+        //1. creates a sub menu that exits/ goes back to previous menu & creates a new directory file using user input.
+        else if(actionPerformed == 1){
+            int subActionPerformed;
+            
+            //submenu
+            printf("1. New directory\n");
+            printf("2. Back\n");
+            printf("9. Exit\n");
+            
+            scanf("%d", &subActionPerformed);
+            if(subMenuOption == 1){
+                printf("Filename: ");
+                scanf("%s", userDirectoryName);
+                createDirectory(userDirectoryName);
+            }
+            else if(subMenuOption == 2){
+                continue;                                               //go to main menu
+            }
+            else if(subMenuOption == 9){
+                break;
+            }
+//            else{
+//                printf("Not an option");
+//                continue;
+//            }
+            //return;
+        }
+        /*
+         //2. creates a new file using user input; additionally creates a sub menu that exits/ goes back to previous menu
+         else if(actionPerformed == 2){
+         printf("New filename: ");
+         scanf("%s", userFilename);
+         createfile(userFilename);
+         
+         //submenu
+         printf("0. Back\n");
+         printf("1. Exit\n");
+         
+         if(subMenuOption == 0){
+         //menu();
+         }
+         else if(subMenuOption == 1){
+         //exit program
+         break;
+         }
+         else{
+         printf("Not an option");
+         break;
+         }
+         
+         
+         
+         //write to a file
+         case 3:
+         printf("Filename: ");
+         scanf("%s", userFilename);
+         writefile(userFilename);
+         break;
+         
+         //read/write to shadow
+         case 4:
+         printf("                     WHAT?                            ");
+         //scan & store
+         //shadow();
+         break;
+         
+         //read an existing file
+         case 5:
+         printf("Filename: ");
+         //scan & store
+         //readfile(userFilename);
+         readfile(userFilename);
+         break;
+         
+         //write to an existing file (in insert, append, or overwite mode)
+         case 6:
+         printf("Filename: ");
+         //scan & store
+         writefile(userFilename);
+         break;
+         
+         //print file status
+         case 7:
+         printf("Filename: ");
+         
+         //scan & store
+         printfilestatus(userFilename);
+         break;
+         
+         //print directory listing
+         case 8:
+         printf("Directory name: ");
+         
+         printdirlisting(userFilename);
+         break;
+         
+         default:
+         printf("Please enter one number between 0-9.\n");
+         }
+         */
+    } while (actionPerformed != 9 && isdigit(actionPerformed));
+}
+    
+int main(int argc, char const *argv[]){
+        
+            menu();
+            scanf("%d", &actionPerformed);
+            performMenuInstruction();
+            
+   
+}
