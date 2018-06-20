35a12a11520bada5a8d823d4981f30bbcad5d61e
LeoTheNeopyte <40406227+LeoTheNeopyte@users.noreply.github.com>
2018-06-20 15:06:30 -0700

    added libraries

--- a2.c
+++ a2.c
@@ -1,8 +1,14 @@
 #include "a2.h"
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <errno.h>
+
 
 int actionPerformed;
 
