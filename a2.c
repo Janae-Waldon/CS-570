606c70ecb7b9da83568bfc240fb38726b5874285
Janae-Waldon <janae.waldon@gmail.com>
2018-06-20 17:17:40 -0700

    last update to a2.c

--- a2.c
+++ a2.c
@@ -1,21 +1,15 @@
 #include "a2.h"
 
-#include "a2.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
+int actionPerformed; // this is the user input
 
-int actionPerformed;
-
-char userFilePath[] = " ";
+char userFilePath[] = " "; //this is to take the files and dirs
 
 void createfile(char* filename){
     FILE *fp;
     fp=fopen(filename,"w");
     fclose(fp);
     return ;
-}
+} //This is the funtion that creates the file
 
 void readfile(char* filename){
     FILE *fp;
@@ -27,7 +21,7 @@ void readfile(char* filename){
         printf("Cannot open file \n");
         exit(0); //TODO make this not exit but return to main menu instead
     }
-    
+
     printf("%s Contains: \n", userFilePath);
     // Read contents from file
     c = fgetc(fp);
@@ -36,37 +30,22 @@ void readfile(char* filename){
         printf ("%c", c);
         c = fgetc(fp);
     }
-    
+
     fclose(fp);
     return;
-}
+} //this is the funtion that reads the file
 
-int writefile(char* filename)
+int writefile(char* filename)// this is the funtion that writes the file
 {
-    FILE * fp;
-    char towrite[80];
-    /* open the file for writing*/
-    fp = fopen (filename,"w");
-    
-    /* write 10 lines of text into the file stream*/
-    scanf ("%[^\n]%*c", towrite);
-    //scanf(stdin, "%s", towrite);
-    fprintf (fp,"%s", towrite);
-    
-    /* close the file*/
-    fclose (fp);
-    
-    //TODO: gotta fork those proccess per assingment request
-    
     pid_t childPID = fork();
-    
+
     if(childPID >= 0) {
-        if(childPID == 0){
-            //TODO: run alpha sort
-        }
-        else{
-            //TODO: run reverse alpha sort
-        }
+      if(childPID == 0){
+          sortalpha(filename);
+      }
+      else{
+          reversesortalpha(filename);
+      }
     }
     else // fork failed
     {
@@ -81,45 +60,60 @@ void append(char* filename){
     char towrite[80];
     //sets a pointer at the end of the file to append text
     fp = fopen(filename, "a");
-    //gets the user input into a buffer, then writes it to the specified file
-    printf("What would you like to append: ");
-    scanf ("%[^\n]%*c", towrite);
-    fprintf (fp,"%s", towrite);
-    
-    fclose(fp);
-}
 
+    if(fp != NULL){
+    //gets the user input into a buffer, then writes it to the specified file
+      printf("What would you like to append: ");
+      scanf (" %[^\n]%*c", towrite);
+      fprintf (fp,"%s", towrite);
+
+      fclose(fp);
+    }
+    else {
+      printf("The File/Path cannot be opened \n");
+    }
+    return;
+} //this well add on to the end of the file
 
 void insert(char* filename, long offset){
     FILE * fp;
     char towrite[80];
     /* open the file for writing */
     fp = fopen (filename,"r+");
-    
-    /* write 10 lines of text into the file stream*/
-    scanf ("%[^\n]%*c", towrite);
-    /* Moves to the specified offset to insert text */
-    fseek(fp, offset, SEEK_SET);
-    fprintf (fp,"%s", towrite);
-    
-    /* close the file*/
-    fclose (fp);
-}
+    if(fp != NULL){
+      /* write 10 lines of text into the file stream*/
+      scanf (" %[^\n]%*c", towrite);
+      /* Moves to the specified offset to insert text */
+      fseek(fp, offset, SEEK_SET);
+      fprintf (fp,"%s", towrite);
+
+      /* close the file*/
+      fclose (fp);
+    }
+    else {
+      printf("The File/Path cannot be opened \n");
+    }
+    return;
+} //this will insert user input into a user specified place
 
 void overWrite(char* filename){
     FILE * fp;
     char towrite[80];
     /* open the file for writing*/
     fp = fopen (filename,"w");
-    
-    /* write 10 lines of text into the file stream*/
-    scanf ("%[^\n]%*c", towrite);
-    //scanf(stdin, "%s", towrite);
-    fprintf (fp,"%s", towrite);
-    
-    /* close the file*/
-    fclose (fp);
-}
+    if(fp != NULL){
+      /* write 10 lines of text into the file stream*/
+      scanf (" %[^\n]%*c", towrite);
+      //scanf(stdin, "%s", towrite);
+      fprintf (fp,"%s", towrite);
+
+      /* close the file*/
+      fclose (fp);
+    }
+    else {
+      printf("The File/Path cannot be opened \n");
+    }
+} //this is used to overwrite the file entirely
 
 void createDirectory(char* dirname){
     int dir;
@@ -134,31 +128,36 @@ void createDirectory(char* dirname){
      S_IXOTH  = others have executable permission
      */
     dir = mkdir(dirname, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH | S_IXUSR | S_IXGRP | S_IXOTH);
-}
+}// this will make a new dir
 
 void printfilestatus(char* filename){
     struct stat info;
     FILE *fp = fopen(filename, "r");
-    int fb = fileno(fp);
-    
-    if (fstat(fb, &info) != 0){
-        perror("Error");
-    } else {
-        puts("File Status:");
-        printf("  inode:   %d\n",   (int) info.st_ino);
-        printf(" dev id:   %d\n",   (int) info.st_dev);
-        printf("   mode:   %08x\n",	  info.st_mode);
-        printf("  links:   %d\n",         info.st_nlink);
-        printf("    uid:   %d\n",   (int) info.st_uid);
-        printf("    gid:   %d\n",   (int) info.st_gid);
+    if(fp != NULL){
+      int fb = fileno(fp);
+
+      if (fstat(fb, &info) != 0){
+          perror("Error");
+        } else {
+          puts("File Status:");
+          printf("  inode:   %d\n",   (int) info.st_ino);
+          printf(" dev id:   %d\n",   (int) info.st_dev);
+          printf("   mode:   %08x\n",	  info.st_mode);
+          printf("  links:   %d\n",         info.st_nlink);
+          printf("    uid:   %d\n",   (int) info.st_uid);
+          printf("    gid:   %d\n",   (int) info.st_gid);
+        }
+        fclose(fp);
     }
-    fclose(fp);
-}
+    else {
+      printf("The File/Path cannot be opened \n");
+    }
+}// this will print the file status
 
 void printdirlisting(char* filedir){
     DIR *dp;
     struct dirent *ep;
-    
+
     dp = opendir (filedir);
     if (dp != NULL){
         printf("Files in Directory: \n");
@@ -170,295 +169,147 @@ void printdirlisting(char* filedir){
         printf("Couldn't open the directory \n");
         printf("\n");
     }
-}
+}//this will print the files inside a ditr
+int cmp(const void *a, const void *b){
+    return strcmp(*(char**)a, *(char**)b);
+} //this is used as a comparer for the sort funtions
+void sortalpha(char* filename){
+  FILE *fp = fopen(filename, "r");
+  char src[50], dest[50];
+  strcpy(dest,filename);
+  strcpy(src,".bak");
+  strcat(dest,src);
 
-void sortalpha(){
-    
-}
 
-void reversesortalpha(){
-    
-}
+  FILE *newAlphaFile=fopen(dest,"w");
+      char text[256];
+      char *words[100];
+      char *word;
+      int wmax=0, i;
+      fscanf(fp,"%[^\0]", text);
+      for(word = strtok(text, " "); word ; word = strtok(NULL, " ")){
+          words[wmax++] = word;
+      }
+      qsort(words, wmax, sizeof(*words), cmp);
+      for(i=0;i<wmax;++i)
+          fprintf(newAlphaFile, "%s ", words[i]);
 
-void menu(){
-    printf("Hello, what would you like to do?\n");
-    printf("1. Create a new directory\n");
-    printf("2. Create a new file\n");
-    printf("3. Read a file\n");
-    printf("4. Write to a file\n");
-    printf("5. Print file status\n");
-    printf("6. Print directory listing\n");
-    printf("0. Exit\n");
-}
+          fclose(fp);
+          fclose(newAlphaFile);
+          return;
 
-void performMenuInstructions(){
-    int subMenuOption;
-    int writeModeMenuOption;
-    int appendMenuOption;
-    
-    do{
-        menu();
-        
-        scanf("%d", &actionPerformed);
-        //        if(!(isdigit(actionPerformed)) ){
-        //            printf("ENTER A NUMBER BETWEEN 0-9\n");
-        //            break;
-        //        }
-        
-        /* 1. Create a new directory:
-         creates a sub menu that exits/ goes back to previous menu & creates a new directory file using user input. */
-        if(actionPerformed == 1){
-            
-            printf("1. Create new directory\n");
-            printf("2. Back\n");
-            printf("0. Exit\n");
-            
-            scanf("%d", &subMenuOption);
-            if(subMenuOption == 1){
-                printf("File Path: ");
-                scanf("%s", userFilePath);
-                createDirectory(userFilePath);
-                continue;
-            }
-            if(subMenuOption == 0){
-                break;
-            }
-            if(subMenuOption == 2){
-                continue;
-            }
-            else{
-                printf("ENTER 1, 2, or 0\n");
-                continue;
-            }
-        }
+}// this will create a new file in sorted order
 
-        /* 2. Create a new file:
-         creates a sub menu to create a new file. Includes the ability to go back to the main menu, exit, create a file by passing user input for filename & path. */
-        else if(actionPerformed == 2){
-            
-            printf("1. Create new file\n");
-            printf("2. Back\n");
-            printf("0. Exit\n");
-            
-            scanf("%d", &subMenuOption);
-            if(subMenuOption == 1){
-                printf("File path: ");
-                scanf("%s", userFilePath);
-                //get path of directory (open)
-                //if (filepath = NULL)
-                //do{
-                //printf("Directory doen't exist!\n");
-                //} while (filepath = NULL);
-                //continue;
-                //cast to directory path
-                createfile(userFilePath);
-                printf("File has been added to filepath.\n");
-                continue;
-            }
-            if(subMenuOption == 0){
-                break;
-            }
-            if(subMenuOption == 2){
-                continue;
-            }
-            else{
-                printf("ENTER 1, 2, or 0\n");
-                printf("\n");
-                continue;
-            } //while (subMenuOption != sucessfulSearch);
-        }
+void reversesortalpha(char* filename){
 
-        /*3. Read an existing file
-         another submenu that reads & opens file and prints the contents to stdout */
-        else if(actionPerformed == 3){
-            
-            printf("1. Read a file\n");
-            printf("2. Back\n");
-            printf("0. Exit\n");
-            
-            scanf("%d", &subMenuOption);
-            if(subMenuOption == 1){
-                printf("File path: ");
-                scanf("%s", userFilePath);
-                readfile(userFilePath);
-                continue;
-            }
-            if(subMenuOption == 0){
-                break;
-            }
-            if(subMenuOption == 2){
-                continue;
-            }
-            else{
-                printf("ENTER 1, 2, or 0\n");
-                continue;
-            }
-        }
-         
-         //write to an existing file (in insert, append, or overwite mode)
-         else if(actionPerformed == 4){
-         
-             printf("1. Write to a file\n");
-             printf("2. Back\n");
-             printf("0. Exit\n");
-         
-             scanf("%d", &subMenuOption);
-             if(subMenuOption == 0){
-                 break;
-             }
-             if(subMenuOption == 2){
-                 continue;
-             }
-             if(subMenuOption == 1){
-                 
-                 printf("WRITE TO FILE MODE\n");
-                 printf("1. Append\n");
-                 printf("2. Insert\n");
-                 printf("3. Overwrite\n");
-                 printf("4. Back\n");
-                 printf("0. Exit\n");
-                 
-                 scanf("%d", &writeModeMenuOption);
-                 if(writeModeMenuOption == 0){
-                     break;
-                 }
-                 if(writeModeMenuOption == 4){
-                     continue;
-                 }
-                 if(writeModeMenuOption == 1){
-                     
-                     printf("1. Append\n");
-                     printf("2. Back\n");
-                     printf("0. Exit\n");
-                     
-                     scanf("%d", &appendMenuOption);
-                     if(appendMenuOption == 1){
-                         printf("Filepath: ");
-                         scanf("%s", userFilePath);
-                         append(userFilePath);
-                         continue;
-                     }
-                     if(writeModeMenuOption == 2){
-                         continue;
-                     }
-                     if(writeModeMenuOption == 0){
-                         break;
-                     }
-                     else{
-                         printf("ENTER 1, 2, or 0\n");
-                         continue;
-                     }
-                     continue;
-                 }
-                 if(writeModeMenuOption == 2){
-                     //insert
-                     printf("1. Insert\n");
-                     printf("2. Back\n");
-                     printf("0. Exit\n");
-                     
-                     scanf("%d", &appendMenuOption);
-                     if(appendMenuOption == 1){
-                         printf("Filepath: ");
-                         scanf("%s", userFilePath);
-                         //insert(userFilePath);
-                         continue;
-                     }
-                     if(appendMenuOption == 2){
-                         continue;
-                     }
-                     if(appendMenuOption == 0){
-                         break;
-                     }
-                     else{
-                         printf("ENTER 1, 2, or 0\n");
-                         continue;
-                     }
-                     continue;
-                 }
-                 if(writeModeMenuOption == 4){
-                     //overWrite
-                     printf("1. Overwrite\n");
-                     printf("2. Back\n");
-                     printf("0. Exit\n");
-                     
-                     scanf("%d", &appendMenuOption);
-                     if(appendMenuOption == 1){
-                         printf("Filepath: ");
-                         scanf("%s", userFilePath);
-                         overWrite(userFilePath);
-                         continue;
-                     }
-                     if(appendMenuOption == 2){
-                         continue;
-                     }
-                     if(appendMenuOption == 0){
-                         break;
-                     }
-                     else{
-                         printf("ENTER 1, 2, or 0\n");
-                         continue;
-                     }
-                     continue;
-                 }
-                 else{
-                     printf("ENTER 1, 2, 3, 4, or 0\n");
-                     continue;
-            }
-        }
-        //print file status
-        else if(actionPerformed == 5){
-            
-            printf("1. Print File Status\n");
-            printf("2. Back\n");
-            printf("0. Exit\n");
-            
-            scanf("%d", &subMenuOption);
-            if(subMenuOption == 1){
-                printf("File path: ");
-                scanf("%s", userFilePath);
+        FILE *fp = fopen(filename, "r");
+        char src[50], dest[50];
+        strcpy(dest,filename);
+        strcpy(src,"reversed.bak");
+        strcat(dest,src);
 
-                printfilestatus(userFilePath);
-                continue;
-            }
-            if(subMenuOption == 0){
-                break;
-            }
-            if(subMenuOption == 2){
-                continue;
-            }
-            else{
-                printf("ENTER 1, 2, or 0\n");
-                continue;
-            }
-        }
-         }
 
-        //print directory listing
-        else if(actionPerformed == 6){
-            
-            printf("1. Print Directory Listing\n");
-            printf("2. Back\n");
-            printf("0. Exit\n");
-            
-            scanf("%d", &subMenuOption);
-            if(subMenuOption == 1){
-                printf("File path: ");
-                scanf("%s", userFilePath);
-            
-                printdirlisting(userFilePath);
-                continue;
+        FILE *newAlphaFile=fopen(dest,"w");
+            char text[100];
+            char *words[100];
+            char *word;
+            int wmax=0, i;
+            fscanf(fp,"%[^\0]", text);
+            for(word = strtok(text, " "); word ; word = strtok(NULL, " ")){
+                words[wmax++] = word;
             }
-            if(subMenuOption == 0){
-                break;
-            }
-            if(subMenuOption == 2){
-                continue;
-            }
-            else{
-                printf("ENTER 1, 2, or 0\n");
-                continue;
-            }
-        }
-         } while (actionPerformed != 0);
-}
+            qsort(words, wmax, sizeof(*words), cmp);
+            for(i=wmax-1;i>0;i--)
+                fprintf(newAlphaFile, "%s ", words[i]);
+      fclose(fp);
+      fclose(newAlphaFile);
+            return;
+}// this will create a new file in reverse sorted order
 
+void mainmenu(){
+  printf("Hello, what would you like to do?\n");
+  printf("1. Create a new directory\n");
+  printf("2. Create a new file\n");
+  printf("3. Read a file\n");
+  printf("4. Write to a file\n");
+  printf("5. Print file status\n");
+  printf("6. Print directory listing\n");
+  printf("0. Exit\n");
+
+
+  scanf(" %d", &actionPerformed);
+  if(actionPerformed == 1){
+    printf("Directoryname: ");
+    scanf(" %s", userFilePath);
+    createDirectory(userFilePath);
+    mainmenu();
+  }
+  if(actionPerformed == 2){
+    printf("File path: ");
+    scanf(" %s", userFilePath);
+    createfile(userFilePath);
+    printf("File has been added to filepath.\n");
+    mainmenu();
+
+  }
+  if(actionPerformed == 3){
+    printf("File path: ");
+    scanf(" %s", userFilePath);
+    readfile(userFilePath);
+    mainmenu();
+  }
+  if(actionPerformed == 4){
+    writefilemenu();
+  }
+  if(actionPerformed == 5){
+    printf("File path: ");
+    scanf(" %s", userFilePath);
+    printfilestatus(userFilePath);
+    mainmenu();
+  }
+  if(actionPerformed == 6){
+    printf("File path: ");
+    scanf(" %s", userFilePath);
+    printdirlisting(userFilePath);
+    mainmenu();
+  }
+  if(actionPerformed == 0){
+    exit;
+  }
+}//this will run through the main menu
+void writefilemenu(){
+  printf("WRITE TO FILE MODE\n");
+  printf("1. Append\n");
+  printf("2. Insert\n");
+  printf("3. Overrite\n");
+  printf("5. Back\n");
+  printf("0. Exit\n");
+  long place=0;
+  scanf(" %d", &actionPerformed);
+  if(actionPerformed == 1){
+    printf("Filepath: ");
+    scanf(" %s", userFilePath);
+    append(userFilePath);
+    writefile(userFilePath);
+  }
+  if(actionPerformed == 2){
+    printf("Filepath: ");
+    scanf(" %s", userFilePath);
+    printf("Place to insert: ");
+    scanf("%ld",&place);
+    insert(userFilePath,place);
+    writefile(userFilePath);
+  }
+  if(actionPerformed == 3){
+    printf("Filepath: ");
+    scanf(" %s", userFilePath);
+    overWrite(userFilePath);
+    writefile(userFilePath);
+  }
+  if(actionPerformed == 5){
+    mainmenu();
+  }
+  if(actionPerformed == 0){
+    exit;
+  }
+} //this will be the submenu for the write choices
